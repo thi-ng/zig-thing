065092bc105145930f4cb0de8c6cdba4930d71a9
Karsten Schmidt <k@thi.ng>
2023-11-26 10:02:52 +0100

    build: update module & pkg name

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("thing", .{
+    _ = b.addModule("thi.ng", .{
         .source_file = .{ .path = "src/main.zig" },
     });
 }
