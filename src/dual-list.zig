873dd4692edbe41677a0252abe6745afc701d58b
Karsten Schmidt <k@thi.ng>
2023-11-28 21:39:08 +0100

    feat: update FixedBufferDualList iterator, add tests & docs

--- src/dual-list.zig
+++ src/dual-list.zig
@@ -33,13 +33,31 @@ pub fn FixedBufferDualList(comptime SIZE: u16, comptime T: type) type {
         const Self = @This();
 
         const Iterator = struct {
-            parent: *const Self,
-            curr: *const T,
+            parent: *Self,
+            nextID: ?T,
 
+            fn init(parent: *Self) Iterator {
+                return Iterator{
+                    .parent = parent,
+                    .nextID = null,
+                };
+            }
+
+            /// Returns next ID in the active list or null if there're no
+            /// further items. Any newly allocated IDs *after* the first
+            /// call to this function will *not* be seen by this iterator
+            /// instance.
+            /// IMPORTANT: Freeing an active ID during iterator processing
+            /// is **only** safe to do for the ID most recently returned by
+            /// this function. Freeing any other ID will result in
+            /// undefined behavior.
             pub fn next(self: *Iterator) ?T {
-                const id = self.curr.*;
-                if (id == SENTINEL) return null;
-                self.curr = &self.parent.slots[id];
+                const id = self.nextID orelse self.parent.active;
+                if (id == SENTINEL) {
+                    self.nextID = SENTINEL;
+                    return null;
+                }
+                self.nextID = self.parent.slots[id];
                 return id;
             }
         };
@@ -93,19 +111,13 @@ pub fn FixedBufferDualList(comptime SIZE: u16, comptime T: type) type {
         }
 
         /// Returns an iterator of all currently active IDs
-        pub fn iterateActive(self: *Self) Iterator {
-            return Iterator{ .parent = self, .curr = &self.active };
-        }
-
-        /// Returns an iterator of all currently free/available IDs (only useful
-        /// for debugging, user code should only need `iterateActive()`)
-        pub fn iterateFree(self: *Self) Iterator {
-            return Iterator{ .parent = self, .curr = &self.available };
+        pub fn iterator(self: *Self) Iterator {
+            return Iterator.init(self);
         }
     };
 }
 
-test "DualList" {
+test "FixedBufferDualList" {
     const ListU8 = FixedBufferDualList(4, u8);
     var list = ListU8.new();
     try testing.expectEqualDeep(
@@ -115,7 +127,7 @@ test "DualList" {
     try testing.expect(list.alloc() == 0);
     try testing.expect(list.alloc() == 1);
     try testing.expect(list.alloc() == 2);
-    var iter = list.iterateActive();
+    var iter = list.iterator();
     while (iter.next()) |i| {
         std.debug.print("active: {d}\n", .{i});
     }
@@ -130,13 +142,74 @@ test "DualList" {
         list,
         ListU8{ .active = 255, .available = 1, .slots = [4]u8{ 3, 2, 0, 255 } },
     );
-    iter = list.iterateFree();
-    while (iter.next()) |i| {
-        std.debug.print("free: {d}\n", .{i});
-    }
     try testing.expect(list.alloc() == 1);
     try testing.expect(list.alloc() == 2);
     try testing.expect(list.alloc() == 0);
     try testing.expect(list.alloc() == 3);
     try testing.expect(list.alloc() == null);
+}
+
+test "FixedBufferDualList.iterator() basic" {
+    const ListU8 = FixedBufferDualList(4, u8);
+    var list = ListU8.new();
+    try testing.expect(list.alloc() == 0);
+    try testing.expect(list.alloc() == 1);
+    try testing.expect(list.alloc() == 2);
+    var iter = list.iterator();
+    try testing.expect(iter.next() == 2);
+    try testing.expect(iter.next() == 1);
+    try testing.expect(iter.next() == 0);
+    try testing.expect(iter.next() == null);
+}
+
+test "iterator alloc post creation" {
+    const ListU8 = FixedBufferDualList(4, u8);
+    var list = ListU8.new();
+    try testing.expect(list.alloc() == 0);
+    try testing.expect(list.alloc() == 1);
+    var iter = list.iterator();
+    try testing.expect(list.alloc() == 2);
+    try testing.expect(iter.next() == 2);
+    // alloc after 1st iter.next() will NOT be seen by iter
+    try testing.expect(list.alloc() == 3);
+    try testing.expect(iter.next() == 1);
+
+    // new iterator
+    iter = list.iterator();
+    // now should see #4...
+    try testing.expect(iter.next() == 3);
+    try testing.expect(iter.next() == 2);
+}
+
+test "iterator free" {
+    const ListU8 = FixedBufferDualList(4, u8);
+    var list = ListU8.new();
+    try testing.expect(list.alloc() == 0);
+    try testing.expect(list.alloc() == 1);
+    try testing.expect(list.alloc() == 2);
+    try testing.expect(list.alloc() == 3);
+
+    var iter = list.iterator();
+    try testing.expect(list.free(3));
+    try testing.expect(iter.next() == 2);
+    try testing.expect(list.free(2));
+    try testing.expect(iter.next() == 1);
+    try testing.expect(list.free(1));
+    try testing.expect(iter.next() == 0);
+    try testing.expect(list.free(0));
+    try testing.expect(list.alloc() == 0);
+    try testing.expect(iter.next() == null);
+    try testing.expect(list.alloc() == 1);
+    try testing.expect(iter.next() == null);
+}
+
+test "iterator empty" {
+    const ListU8 = FixedBufferDualList(4, u8);
+    var list = ListU8.new();
+    var iter = list.iterator();
+    try testing.expect(iter.next() == null);
+    // once end has been reached, new allocs must not impact iterator
+    // checking a special case here since list is empty so next() call is different
+    try testing.expect(list.alloc() == 0);
+    try testing.expect(iter.next() == null);
 }
