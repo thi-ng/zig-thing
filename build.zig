a398a8a5562a8f10883bf02836a08db05f67f0dd
Karsten Schmidt <k@thi.ng>
2024-04-24 10:11:35 +0200

    build: update build.zig & version bump

--- build.zig
+++ build.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("thi.ng", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 }
