7b0ff986a9bec4f6c7063c14cbd670be87f5b10b
Karsten Schmidt <k@thi.ng>
2025-06-22 00:23:22 +0200

    build: update build info & module name

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("thi.ng", .{
+    _ = b.addModule("thing", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
